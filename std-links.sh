33a0a5a17c5b4b7d218ab67c0985d963dbcc5a73
seth <seth@localhost>
2013-05-16 09:17:29 -0500

    Standard linking script for X-based Linux platforms

---
+++ std-links.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This script sets up some standard symlinks for sbp-linux-config. These links should be
+# applicable to all X-based Linux desktops.
+
+ln -s `realpath ./_.Xresources` ~/.Xresources
+
