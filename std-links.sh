2c5c2ec3c8fe8696e8c88d662d62afe8637e2c8f
seth <seth@localhost>
2013-05-16 09:27:34 -0500

    Added vimrc

--- std-links.sh
+++ std-links.sh
@@ -2,5 +2,11 @@
 # This script sets up some standard symlinks for sbp-linux-config. These links should be
 # applicable to all X-based Linux desktops.
 
-ln -s `realpath ./_.Xresources` ~/.Xresources
+make_link() {
+  echo Linking `realpath $1` as $2 ...
+  ln -sf `realpath $1` $2
+}
+
+make_link ./_.Xresources ~/.Xresources
+make_link ./_.vimrc ~/.vimrc
 
