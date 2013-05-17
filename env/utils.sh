c0e021cb97c3e626b77d1b9e9c7875c488d7f161
seth <seth@localhost>
2013-05-17 13:05:47 -0500

    Backed out changeset: dc0c3411a3d4 It seems to be just a kolvillag problem with pm-suspend.

--- env/utils.sh
+++ env/utils.sh
@@ -102,7 +102,8 @@ alias la='ls -A'
 alias l='ls -CF'
 
 # Command-line aliases for DE functionality.
-alias i3lock="i3lock -c 0004D4"
+alias i3lock="i3lock -c 000000"
+alias i3suspend="sudo pm-suspend && i3lock"
 
 
 ###############################################################################
