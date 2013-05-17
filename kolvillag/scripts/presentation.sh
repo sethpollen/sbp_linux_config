244441c7bd0bcda7c4cac92f3ac9262d9ec11971
seth <seth@localhost>
2013-05-17 08:43:05 -0500

    Standard xrandr scripts for kolvillag

---
+++ kolvillag/scripts/presentation.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Configures the display to use my external monitor setup at home.
+
+xrandr --output LVDS-1 --mode "1280x1024" --primary
+xrandr --output VGA-1 --mode "1280x1024" --same-as LVDS-1
