25a6b84e7056e83a038cb3945c35c6db8d0392a6
seth <seth@localhost>
2013-05-18 06:57:41 -0500

    Auto-setting of xrandr

--- autorun.d/detect-displays.sh
+++ autorun.d/detect-displays.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
-# Tries to auto-detect the current display hardware and choose the appropriate
-# config script.
+# Tries to auto-detect the current display hardware and choose the
+# appropriate config script.
 
 # Tries to use xrandr's command-line output to see if the display output $1
 # is currently connected.
 is_connected() {
-  if xrandr --query | grep "$1 connected"; then
-    #TODO
+  if xrandr --query | grep --silent "$1 connected" ; then
+    return 0
+  else
+    return 1
   fi
 }
 
+# Based on the host, we choose the name of the external monitor which
+# determines the configuration to use.
 HOST=`hostname`
 case "$HOST" in
   kolvillag)
-    # TODO
+    SECOND_DISPLAY="VGA-1"
     ;;
 esac
+
+# Now test if the external monitor is connected, and run the appropriate
+# script.
+if is_connected $SECOND_DISPLAY ; then
+  $SBP_LINUX_CONFIG/hosts/$HOST/two-monitors.sh
+else
+  $SBP_LINUX_CONFIG/hosts/$HOST/one-monitor.sh
+fi
