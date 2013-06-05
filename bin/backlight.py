5aa2ac8971bc7f45651e46c12b318a1d45bf1469
pollen <pollen@localhost>
2013-06-05 05:36:45 -0700

    Python script for backlight manipulations now uses xbacklight whenever possible.

--- bin/backlight.py
+++ bin/backlight.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-# Library of routines for manipulating the backlight through /sys.
+# Library of routines for manipulating the backlight we always begin by trying
+# to use xbacklight, falling back on /sys if that doesn't work.
 
 import os
+import subprocess
   
 def writeFile(name, contents):
   with open(name, 'w') as f:
@@ -24,6 +26,13 @@ for child in os.listdir(searchRoot):
 
 def setBrightness(fraction):
   """ Sets the brightness. Fraction should be between 0 and 1. """
+  try:
+    subprocess.check_call(['xbacklight', '-set', str(round(fraction * 100))])
+    return # Success.
+  except subprocess.CalledProcessError:
+    pass # Ignore.
+  
+  # xbacklight failed; try using /sys.
   if brightnessFile: # We found a backlight to manipulate.
     if fraction < 0.0:
       fraction = 0.0
@@ -34,6 +43,13 @@ def setBrightness(fraction):
     
 def getBrightness():
   """ Gets the brightness, as a fraction between 0 and 1. """
+  try:
+    result = subprocess.check_output(['xbacklight', '-get'])
+    return float(result) * 0.01
+  except subprocess.CalledProcessError:
+    pass # Ignore
+  
+  # xbacklight failed; try using /sys.
   if brightnessFile: # We found a backlight to manipulate.
     return readFile(brightnessFile) / maxBrightness
   else:
