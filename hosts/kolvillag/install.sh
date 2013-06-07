11855e997b7a1ec81a3fcb2b79f9e8a1217c89ca
pollen <pollen@localhost>
2013-06-06 21:33:26 -0700

    rename tweaks -> src

--- hosts/kolvillag/install.sh
+++ hosts/kolvillag/install.sh
@@ -2,5 +2,5 @@
 # Wrapper for install.sh which provides kolvillag-specific configurations.
 
 SBP_LINUX_CONFIG=~/sbp-linux-config
-TWEAKS=$SBP_LINUX_CONFIG/hosts/kolvillag/tweaks
-$SBP_LINUX_CONFIG/install.sh $TWEAKS
+SRC=$SBP_LINUX_CONFIG/hosts/kolvillag/src
+$SBP_LINUX_CONFIG/install.sh $SRC
