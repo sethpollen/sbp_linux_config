bc407ffca297dd53b34e34edbf0fbf6e3cafab7b
pollen <pollen@localhost>
2013-06-11 11:21:32 -0500

    Improved laptop design

--- hosts/kolvillag/install.py
+++ hosts/kolvillag/install.py
@@ -11,16 +11,4 @@ LOCAL_SRC = p.join(SBP_LINUX_CONFIG, 'hosts/kolvillag/src')
 
 install = imp.load_source('install', p.join(SBP_LINUX_CONFIG, 'install.py'))
 install.standard([LOCAL_SRC])
-
-# Read in the i3status.conf constructed so far.
-I3STATUS_CONF = p.join(install.BIN, 'dotfiles/i3status.conf')
-with open(I3STATUS_CONF) as f:
-  conf = f.read()
-
-print 'Inserting Battery entry into i3status.conf ...'
-conf = install.insertBefore(conf,
-    'order += "cpu_usage"', 'order += "battery 0"')
-
-# Write out the new i3status.conf lines.
-with open(I3STATUS_CONF, 'w') as f:
-  f.write(conf)
+install.standardLaptop()
