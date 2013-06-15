d6bf10d80de4f7fe7a54943d7d3933a75bf59d26
pollen <pollen@localhost>
2013-06-15 14:06:04 -0500

    Configuration improvements learned from kolvillag reimage.

--- hosts/kolvillag/src/scripts/two-monitors.sh
+++ hosts/kolvillag/src/scripts/two-monitors.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Configures the display to use my external monitor setup at home.
 
+set-up-x-modes.sh
+
 xrandr --output LVDS-1 --mode "1440x900" --primary
 xrandr --output VGA-1 --mode "1280x1024" --right-of LVDS-1
