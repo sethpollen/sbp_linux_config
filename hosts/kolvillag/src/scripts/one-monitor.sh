debe5f258beb13ee5ebea0cea8ed40e0cc904b46
pollen <pollen@localhost>
2013-06-15 14:06:04 -0500

    Configuration improvements learned from kolvillag reimage.

--- hosts/kolvillag/src/scripts/one-monitor.sh
+++ hosts/kolvillag/src/scripts/one-monitor.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Configures the display to use just the integrated flatpanel.
 
+set-up-x-modes.sh
+
 xrandr --output LVDS-1 --mode "1440x900" --primary
 xrandr --output VGA-1 --off
--- hosts/kolvillag/src/scripts/presentation.sh
+++ hosts/kolvillag/src/scripts/presentation.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Configures the display to use a projector for presentations.
 
+set-up-x-modes.sh
+
 xrandr --output LVDS-1 --mode "1280x1024" --primary
 xrandr --output VGA-1 --mode "1280x1024" --same-as LVDS-1
