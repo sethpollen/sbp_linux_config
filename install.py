88d0733272dfe5b9d56f336c86a47530fbdc597e
pollen <pollen@localhost>
2013-06-17 16:30:15 -0500

    kspawn

---
+++ install.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# This script provides a standard installation of sbp-linux-config through
+# the following steps:
+#   1. It copies everything from ./src to ./bin.
+#   2. It makes several symlinks in standard places (such as ~) that point
+#      to the appropriate files in ./bin.
+#   3. If arguments are provided, each is interpreted as a directory which
+#      may contain zero or more subdirectories corresponding to the
+#      subdirectories of ./src. Each file in each of these directires is
+#      read in and appended to the corresponding file in ./bin. If no such
+#      file exists yet in ./bin, it is created with the appended contents.
+#      This provides a simple mechanism for adding per-machine customizations.
+
+import os
+import os.path as p
+import shutil
+import sys
+import subprocess
+
+HOME = p.expanduser('~')
+SBP_LINUX_CONFIG = p.join(HOME, 'sbp-linux-config')
+SRC = p.join(SBP_LINUX_CONFIG, 'src')
+BIN = p.join(SBP_LINUX_CONFIG, 'bin')
+DOTFILES_BIN = p.join(BIN, 'dotfiles')
+SCRIPTS_BIN = p.join(BIN, 'scripts')
+I3STATUS_CONF = p.join(BIN, 'dotfiles/i3status.conf')
+
+# Some utility methods for other install scripts to use for manipulating the
+# output of this script:
+
+def readFile(name):
+  with open(name) as f:
+    return f.read()
+
+def writeFile(name, text):
+  with open(name, 'w') as f:
+    f.write(text)
+
+def insertBefore(text, afterLine, newLine):
+  """ Inserts newLine into text, right before afterLine. """
+  lines = text.splitlines()
+  lineNum = lines.index(afterLine)
+  lines.insert(lineNum, newLine)
+  return '\n'.join(lines)
+
+
+def standard(appendDirs):
+  """ Invokes the standard install procedure. """
+
+  # Helper function.
+  def forceLink(target, linkName):
+    """ Forces a symlink, even if the linkName already exists. """
+    if p.islink(linkName) or p.isfile(linkName):
+      os.remove(linkName)
+
+    # Don't handle the case where linkName is a directory--it's too easy to
+    # blow away existing config folders that way.
+
+    print 'Linking %s as %s ...' % (target, linkName)
+    os.symlink(target, linkName)
+
+  # Clean out any existing bin stuff.
+  if p.isdir(BIN):
+    shutil.rmtree(BIN)
+
+  # Perform the copy.
+  shutil.copytree(SRC, BIN)
+
+  # Recursive helper for linking over individual files in the tree rooted at
+  # dotfiles.
+  def linkDotfiles(targetDir, linkDir, addDot):
+    if not p.exists(linkDir):
+      os.mkdir(linkDir)
+    for childName in os.listdir(targetDir):
+      targetChild = p.join(targetDir, childName)
+ 
+      linkChildName = '.' + childName if addDot else childName
+      linkChild = p.join(linkDir, linkChildName)
+
+      if p.isfile(targetChild):
+        forceLink(targetChild, linkChild)
+      elif p.isdir(targetChild):
+        # Recurse, and don't add any more dots.
+        linkDotfiles(targetChild, linkChild, False)
+
+  # Call the recursive function.
+  linkDotfiles(DOTFILES_BIN, HOME, True)
+
+  # Link in all the other scripts that should be on the path.
+  forceLink(SCRIPTS_BIN, p.join(HOME, 'bin'))
+
+  # Process arguments to see if they contain append-files.
+  for appendDir in appendDirs:
+    if not p.isdir(appendDir):
+      print 'ERROR: %s is not a directory.' % appendDir
+    else:
+      # Look at every file in the appendDir.
+      for root, dirs, files in os.walk(appendDir):
+        # Make root relative to the appendDir, since we'll want to use it both in
+        # the appendDir and in BIN.
+        root = p.relpath(root, appendDir)
+        for fil in files:
+          # Compute the full path from the appendDir to the file.
+          fil = p.join(root, fil)
+
+          appendSource = p.join(appendDir, fil)
+          appendDest = p.join(BIN, fil)
+
+          if p.exists(appendDest):
+            print 'Appending %s to %s ...' % (appendSource, appendDest)
+            with open(appendDest) as f:
+              text = f.read()
+            while not text.endswith('\n\n'):
+              text += '\n'
+            with open(appendSource) as f:
+              text += f.read()
+            with open(appendDest, 'w') as f:
+              f.write(text)
+          else:
+            print 'Copying %s to %s ...' % (appendSource, appendDest)
+            shutil.copy(appendSource, appendDest)
+
+  # Prevent GNOME's nautilus from leaving behind those weird "Desktop" windows:
+  subprocess.call(['gsettings', 'set', 'org.gnome.desktop.background',
+      'show-desktop-icons', 'false'])
+
+
+def standardLaptop():
+  """ Meant to be invoked after standard() for laptops. Adds some useful
+  configuration settings for laptops.
+  """
+  text = readFile(I3STATUS_CONF)
+
+  print 'Inserting Battery entry into i3status.conf ...'
+  text = insertBefore(text, 'order += "cpu_usage"', 'order += "battery 0"')
+
+  print 'Inserting Wi-Fi entry into i3status.conf ...'
+  text = insertBefore(text,
+      'order += "ethernet eth0"', 'order += "wireless wlan0"')
+
+  writeFile(I3STATUS_CONF, text)
+
+
+if __name__ == '__main__':
+  standard(sys.argv[1:])
---
+++ src/dotfiles/shell-setup.sh
@@ -0,0 +1,120 @@
+###############################################################################
+# SHELL VARIABLES
+###############################################################################
+
+# We used to define some standard shell variables here (like $EDITOR), but we
+# found that it's much better to put these in /etc/environment, where they will
+# be available to all programs, not just to zsh instances.
+
+###############################################################################
+# SHELL PROMPT AND TITLE
+###############################################################################
+
+# zsh invokes the precmd function before each prompt.
+precmd() {
+  # Fancy PWD display function:
+  # The home directory (HOME) is replaced with a ~.
+  # The last pwdmaxlen characters of the PWD are displayed.
+  # Leading partial directory names are striped off:
+  # /home/me/stuff          -> ~/stuff               if USER=me
+  # /usr/share/big_dir_name -> ../share/big_dir_name if pwdmaxlen=20
+
+  # Grab the exit code of the last command before messing it up.
+  local EXIT_CODE=$?
+  if [ ${EXIT_CODE} -eq "0" ]; then
+    # Don't show zero exit codes.
+    EXIT_CODE=
+  else
+    # Wrap exit codes in brackets.
+    EXIT_CODE="[${EXIT_CODE}]"
+  fi
+  
+  # How many characters of the $PWD should be kept
+  local pwdmaxlen=50
+  # Indicate that there has been dir truncation
+  local trunc_symbol=".."
+  local dir=${PWD##*/}
+  local pwdmaxlen=$(( ( pwdmaxlen < ${#dir} ) ? ${#dir} : pwdmaxlen ))
+  NEW_PWD=${PWD/#$HOME/\~}
+  local pwdoffset=$(( ${#NEW_PWD} - pwdmaxlen ))
+  if [ ${pwdoffset} -gt "0" ]
+  then
+    if [ $ZSH_NAME ]; then
+      # zsh uses a different syntax for grabbing substrings.
+      NEW_PWD=$NEW_PWD[$pwdoffset,9999]
+    else
+      NEW_PWD=${NEW_PWD:$pwdoffset:$pwdmaxlen}
+    fi
+    NEW_PWD=${trunc_symbol}/${NEW_PWD#*/}
+  fi
+  
+  local PROMPT_DOLLAR_COLOR="1;93m" # Bold yellow.
+  local EXIT_CODE_COLOR="\e[1;91m" # Bold red.
+  local PROMPT_COLOR="\e[1;96m" # Bold cyan.
+  local COMMAND_COLOR="0m"
+
+  # Set up command prompt line.
+  PS1="\e[${PROMPT_COLOR}%D{%m/%d %H:%M} %m ${NEW_PWD}"
+  PS1="${PS1} ${EXIT_CODE_COLOR}${EXIT_CODE}\n"
+  PS1="${PS1}\e[${PROMPT_DOLLAR_COLOR}z> \e[${COMMAND_COLOR}"
+  
+  # For some reason, we need this line to get zsh to recognize the
+  # backslash-e escapes.
+  PS1=$(echo $PS1)
+
+  # Set the xterm title bar to contain hostname and shortened cwd.
+  case "$TERM" in
+    xterm*)
+      print -Pn "\e]0;%m ${NEW_PWD}\a"
+  esac
+}
+
+# Bash doesn't use precmd; it instead uses the PROMPT_COMMAND variable.
+PROMPT_COMMAND=precmd
+
+
+###############################################################################
+# ALIASES
+###############################################################################
+
+# Some nice shortcuts.
+alias fd..="fd .."
+alias ..="fd .."
+alias gist="git status"
+alias gibt="git branch"
+
+# Turn on coloring for some commands.
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Some more ls aliases.
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+
+###############################################################################
+# UTILITY FUNCTIONS
+###############################################################################
+
+append_to_path() {
+  # This works in zsh and bash.
+  export PATH="$PATH:$1"
+}
+
+# File browsing.
+fd() {
+  if [ $# -ge 1 ]; then
+    if [ -f "$1" ]; then
+      vim "$1"
+    fi
+    if [ -d "$1" ]; then
+      cd "$1"
+      ls --color=always
+    fi
+  else
+    ls --color=always
+  fi
+}
