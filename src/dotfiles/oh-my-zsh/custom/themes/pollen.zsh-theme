b523234d4763b62d016dca6c91c2fac1fd51cdd3
pollen <pollen@localhost>
2013-06-21 21:47:15 -0500

    Working on it...

---
+++ src/dotfiles/oh-my-zsh/custom/themes/pollen.zsh-theme
@@ -0,0 +1,41 @@
+# My personal oh-my-zsh theme.
+
+clear_cwd_file() {
+  # When the shell exits, clear the remembered cwd.
+  rm -f ~/.cwd
+}
+autoload -U add-zsh-hook
+add-zsh-hook zshexit clear_cwd_file
+
+# Prompt colors.
+cyan="%{$fg_bold[cyan]%}"
+yellow="%{$fg_bold[yellow]%}"
+red="%{$fg_bold[red]%}"
+white="%{$fg_bold[white]%}"
+noColor="%{$reset_color%}"
+
+# Use single quotes when you want to defer evaluation.
+prompt_dateTime="${cyan}%D{%m/%d %H:%M}"
+prompt_host="${cyan}%m"
+prompt_pwd="${cyan}%50<..<%~%<<"
+prompt_returnCode="%(?..${red}[%?])"
+prompt_arrow="${yellow}>"
+
+# Callable function for making the default prompt.
+default_prompt() {
+  export PROMPT="${prompt_dateTime} ${prompt_host} ${prompt_pwd} ${prompt_returnCode}
+${prompt_arrow} ${noColor}"
+}
+
+# Make the default prompt.
+default_prompt
+
+# Callable function for making the default titlebars.
+default_titleBar() {
+  local titleBar="%m %50<..<%~%<<"
+  export ZSH_THEME_TERM_TAB_TITLE_IDLE=$titleBar
+  export ZSH_THEME_TERM_TITLE_IDLE=$titleBar
+}
+
+# Make the default titlebars.
+default_titleBar
---
+++ src/dotfiles/oh-my-zsh/custom/utils.sh
@@ -0,0 +1,62 @@
+# Extra zsh code to run whenever a new shell opens. This includes some standard
+# functions and aliases, as well as a few bits of init logic.
+
+# A few useful functions.
+append_to_path() {
+  # This works in zsh and bash.
+  export PATH="$PATH:$1"
+}
+
+# File browsing.
+fd() {
+  if [ $# -ge 1 ]; then
+    if [ -f "$1" ]; then
+      vim "$1"
+    fi
+    if [ -d "$1" ]; then
+      cd "$1"
+      ls --color=always
+    fi
+  else
+    ls --color=always
+  fi
+}
+
+# Some nice shortcuts.
+alias fd..="fd .."
+alias ..="fd .."
+alias gist="git status"
+alias gibt="git branch"
+
+# Turn on coloring for some commands.
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Some more ls aliases.
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Move the shell to the last known path.
+if [ -e ~/.cwd ]; then
+  local dest=$(cat ~/.cwd)
+  if [ -d "$dest" ]; then
+    cd $dest
+  fi
+
+  # Clear variables to keep them from cluttering things up.
+  unset dest
+fi
+
+# Function to switch and save the current path.
+cd() {
+  builtin cd "$@";
+  echo "$PWD" > ~/.cwd;
+}
+
+# I don't like zsh's "named directories" feature; for some reason it picks
+# up my environment variables and pollutes my %~ prompt expansion. So here we
+# issue a command that should clear all the named directories.
+unhash -md "*"
