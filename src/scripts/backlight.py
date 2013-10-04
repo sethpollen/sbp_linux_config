8b504193bf56d7fcd02c5286d03219624ec65409
pollen <pollen@localhost>
2013-10-03 22:13:09 -0500

    cklight

--- src/scripts/backlight.py
+++ src/scripts/backlight.py
@@ -50,39 +50,43 @@ class SysBacklight:
       return -1 # Error.
 
 
-# Singleton instance (lazily initialized).
-sysBacklight = None
-
-def getSysBacklight():
-  if not sysBacklight:
-    sysBacklight = SysBacklight()
-  return sysBacklight
-
-
-def setBrightness(fraction):
-  """ Sets the brightness. Tries xbacklight and falls back on /sys.
-  Fraction should be between 0 and 1.
+class Backlight:
+  """ Wrapper for everything else in this file. This class decides internally
+  how to access the backlight. It prefers xbacklight, but it will fall back
+  on /sys if it has to.
   """
-  try:
-    subprocess.check_call(['xbacklight', '-set', str(round(fraction * 100))])
-    return # Success.
-  except subprocess.CalledProcessError:
-    pass # Ignore.
+  def __init__(self):
+    # Singleton instance of SysBacklight (lazily initialized).
+    self.fallback = None
+
+  def getFallback(self):
+    if not self.fallback:
+      fallback = SysBacklight()
+    return fallback
+
+  def setBrightness(self, fraction):
+    """ Sets the brightness. Tries xbacklight and falls back on /sys.
+    Fraction should be between 0 and 1.
+    """
+    try:
+      subprocess.check_call(['xbacklight', '-set', str(round(fraction * 100))])
+      return # Success.
+    except subprocess.CalledProcessError:
+      pass # Ignore.
   
-  # xbacklight failed; try using /sys.
-  getSysBacklight().setBrightness(fraction)
-      
+    # xbacklight failed; try using /sys.
+    self.getFallback().setBrightness(fraction)
     
-def getBrightness():
-  """ Gets the brightness, as a fraction between 0 and 1. Tries xbacklight and
-  falls back on /sys.
-  """
-  try:
-    result = subprocess.check_output(['xbacklight', '-get'])
-    if result:
-      return float(result) * 0.01
-  except subprocess.CalledProcessError:
-    pass # Ignore
-  
-  # xbacklight failed; try using /sys.
-  return getSysBacklight().getBrightness()
+  def getBrightness(self):
+    """ Gets the brightness, as a fraction between 0 and 1. Tries xbacklight and
+    falls back on /sys.
+    """
+    try:
+      result = subprocess.check_output(['xbacklight', '-get'])
+      if result:
+        return float(result) * 0.01
+    except subprocess.CalledProcessError:
+      pass # Ignore
+    
+    # xbacklight failed; try using /sys.
+    return self.getFallback().getBrightness()
