1c2c8d38d91c57bad726f63bdffc81e664c62bad
pollen <pollen@localhost>
2014-02-26 23:35:08 -0600

    TODOs

---
+++ src/python/i3sbp.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+# Library of routines for manipulating the i3 window manager (http://i3wm.org).
+
+import subprocess
+import json
+import dmenu
+import sys
+
+
+# Will store JSON workspace tree.
+workspaces = None
+
+# Will store currently focused workspace's JSON tree.
+currentWorkspace = None
+
+# Will map workspace numbers to workspace JSON tree.
+workspacesByNumber = None
+
+
+def getWorkspaces():
+  """ Grabs the workspaces JSON tree from i3. """
+  global workspaces
+  if not workspaces:
+    workspaces = json.loads(
+        subprocess.check_output(['i3-msg', '-t', 'get_workspaces']))
+  return workspaces
+  
+  
+def getWorkspacesByNumber():
+  global workspacesByNumber
+  if not workspacesByNumber:
+    workspacesByNumber = {}
+    for w in getWorkspaces():
+      workspacesByNumber[w['num']] = w
+  return workspacesByNumber
+  
+  
+def getCurrentWorkspace():
+  global currentWorkspace
+  if not currentWorkspace:
+    for w in getWorkspaces():
+      if w['focused']:
+        currentWorkspace = w
+        break
+  return currentWorkspace
+  
+  
+def name(w):
+  """ Gets the name from a workspace JSON tree. """
+  return w['name'] if w else None
+  
+  
+def num(w):
+  """ Gets the number from a workspace JSON tree. """
+  return w['num'] if w else None
+  
+  
+def output(w):
+  """ Gets the output (screen) from a workspace JSON tree. """
+  return w['output'] if w else None
+
+
+def i3msg(commands):
+  """ Calls i3-msg with the given i3 commands. 'commands' may be a string or
+  list of strings. Throws an exception if i3-msg exits with a non-zero status.
+  """
+  if type(commands) is list:
+    commands = ' ; '.join(commands)
+  return subprocess.check_call(['i3-msg', '-q', commands])
+  
+
+def getFreeWorkspaceNumber():
+  """ Gets the smallest free workspace number. """
+  free = 1
+  while free in getWorkspacesByNumber():
+    free += 1
+  return free
+
+
+def makeWorkspaceName(number, name):
+  """ Builds a complete workspace name from a number and a string. """
+  if not name:
+    # Don't insert colons needlessly.
+    return str(number)
+  else:
+    return "%d:%s" % (number, name)
+    
+    
+def makeWorkspaceSpecifier(nameOrNumber):
+  """ Makes the i3 command text to specify a workspace, either using its
+  full name or just its number.
+  """
+  if type(nameOrNumber) is int:
+    return 'workspace number %d' % nameOrNumber
+  else:
+    return 'workspace "%s"' % nameOrNumber
+    
+    
+def parseWorkspaceNumber(workspace):
+  """ Parses and returns the numeric portion of a workspace name. If no
+  number is found, returns -1.
+  """
+  parts = workspace.split(':', 1)
+  try:
+    return int(parts[0])
+  except ValueError:
+    return -1
+    
+    
+def removeWorkspaceNumber(workspace):
+  """ Removes the numeric portion of a workspace name. """
+  parts = workspace.split(':', 1)
+  if len(parts) == 1:
+    try:
+      dummy = int(parts[0])
+      # It's just a number.
+      return None
+    except ValueError:
+      return parts[0]
+  else:
+    return parts[1]
+
+def chooseWorkspace(prompt):
+  """ Prompts the user to choose a workspace from a dmenu of existing
+  workspaces. If the user cancels, returns None. If the user's choice doesn't
+  match any existing workspaces and doesn't begin with a number,
+  the next free number is prepended to their selection.
+  """
+  selection = dmenu.dmenu(prompt, [name(w) for w in getWorkspaces()])
+  if not selection:
+    return None
+  number = parseWorkspaceNumber(selection)
+  if number < 0:
+    # User didn't specify a number at all.
+    return makeWorkspaceName(getFreeWorkspaceNumber(), selection)
+  if number in getWorkspacesByNumber():
+    # The user specified a number which is already in use. Even if they
+    # specified a different name, clamp them to the workspace with that
+    # number.
+    for w in getWorkspaces():
+      if number == num(w):
+        return name(w)
+  # If all else fails, just return the user's selection.
+  return selection
+    
+
+def enterNewWorkspaceName(prompt, currentNumber):
+  """ Prompts the user to enter a new name for workspace with number
+  'currentNumber'. If the user choice doesn't begin with a number,
+  'currentNumber' is prepended.
+  """
+  selection = dmenu.dmenu(prompt, [])
+  if not selection:
+    return None
+  number = parseWorkspaceNumber(selection)
+  if number < 0:
+    # User didn't specify a number at all, so just keep the current one.
+    return makeWorkspaceName(currentNumber, selection)
+  if number in getWorkspacesByNumber() and number is not currentNumber:
+    # This number is already in use elsewhere, so we can't use it here.
+    return None
+  return selection
+  
+  
+def getAdjacentWorkspace(direction):
+  """ Gets the JSON tree of the workspace adjacent to the current workspace.
+  'direction' should be -1 (for left) or 1 (for right). Will only return a
+  workspace on the same screen as the current workspace.
+  """
+  currentW = getCurrentWorkspace()
+  i = num(currentW) + direction
+  allW = getWorkspacesByNumber()
+  maxNumber = max(allW.keys())
+  minNumber = min(allW.keys())
+  while i >= minNumber and i <= maxNumber:
+    if i in allW:
+      otherW = allW[i]
+      if output(otherW) == output(currentW):
+	return otherW
+    i += direction
+  # We ran out of workspaces.
+  return None
+    
+
+def focusWorkspace(nameOrNumber):
+  """ Focuses workspace with the given string name or integer number. """
+  if nameOrNumber:
+    i3msg(makeWorkspaceSpecifier(nameOrNumber))
+    
+    
+def moveToWorkspace(nameOrNumber):
+  """ Moves the current container to workspace with the given string name or
+  integer number.
+  """
+  if nameOrNumber:
+    i3msg('move container to ' + makeWorkspaceSpecifier(nameOrNumber))
+
+
+def moveToScratchpad():
+  """ Moves the current window to the scratchpad. """
+  i3msg('move scratchpad')
+    
+    
+def renameCurrentWorkspace(newName):
+  """ Renames the current workspace to 'newName'. """
+  if newName:
+    i3msg('rename workspace "%s" to "%s"' %
+          (name(getCurrentWorkspace()), newName))
+          
+
+def swapNumbers(workspace1, workspace2):
+  """ Swaps the numbers of two workspaces, whose JSON trees must be given. """
+  if workspace1 and workspace2:
+    num1 = num(workspace1)
+    name1 = removeWorkspaceNumber(name(workspace1))
+    num2 = num(workspace2)
+    name2 = removeWorkspaceNumber(name(workspace2))
+    i3msg(('rename workspace "%s" to 999999; ' +
+           'rename workspace "%s" to "%s"; ' +
+           'rename workspace 999999 to "%s"') % (
+             name(workspace1),
+             name(workspace2),
+             makeWorkspaceName(num1, name2),
+             makeWorkspaceName(num2, name1)))
+    
+
+## ENTRY POINTS ##
+# Intended to be bound to keys.
+
+def switchChoose():
+  focusWorkspace(chooseWorkspace('Switch to workspace:'))
+  
+def moveChoose():
+  moveToWorkspace(chooseWorkspace('Move to workspace:'))
+  
+def switchNew():
+  focusWorkspace(getFreeWorkspaceNumber())
+
+def moveNew():
+  number = getFreeWorkspaceNumber()
+  moveToWorkspace(number)
+  focusWorkspace(number)
+  
+def rename():
+  renameCurrentWorkspace(enterNewWorkspaceName('New workspace name:',
+                                               num(getCurrentWorkspace())))
+
+def swapLeft():
+  swapNumbers(getCurrentWorkspace(),
+              getAdjacentWorkspace(-1))
+              
+def swapRight():
+  swapNumbers(getCurrentWorkspace(),
+              getAdjacentWorkspace(1))
---
+++ src/python/netusage.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+# Library for reading network usage stats from /proc/net/dev.
+
+import time
+import os
+
+STATS_FILE = '/proc/net/dev'
+
+WIRELESS_INTERFACE = 'wlan0'
+ETHERNET_INTERFACE = 'eth0'
+
+
+class Rate:
+  """ Computes a instantaneous rate of a counter. """
+
+  def __init__(self):
+    self.lastCount = 0
+    # If we start with the timestamp at -inf, the first rate computation will
+    # still produce a rate of zero.
+    self.lastTimestamp = float('-inf')
+    self.rate = 0
+
+  def update(self, now, count):
+    count = float(count)
+    now = float(now)
+    self.rate = (count - self.lastCount) / (now - self.lastTimestamp)
+    self.lastCount = count
+    self.lastTimestamp = now
+
+
+class InterfaceStats:
+  """ Contains stats for a single network interface. """
+  
+  def __init__(self):
+    self.rxBytes = Rate()
+    self.txBytes = Rate()
+
+  def update(self, now, rxByteCount, txByteCount):
+    self.rxBytes.update(now, rxByteCount)
+    self.txBytes.update(now, txByteCount)
+
+
+class Stats:
+  """ Contains stats parsed from /proc/net/dev. """
+
+  def __init__(self):
+    self.interfaces = {}
+    self.update()
+
+  def update(self):
+    """ Updates rates by reading /proc/net/dev. """
+    now = time.time()
+    with open(STATS_FILE, 'r') as f:
+      lines = f.readlines()
+      headerLine = lines[1]
+      _, receiveCols , transmitCols = headerLine.split("|")
+      receiveCols = map(lambda a: "rx_" + a, receiveCols.split())
+      transmitCols = map(lambda a: "tx_" + a, transmitCols.split())
+      cols = receiveCols + transmitCols
+
+      for line in lines[2:]:
+        if line.find(":") < 0: continue
+        interfaceName, data = line.split(":")
+        interfaceName = interfaceName.strip()
+        interfaceData = dict(zip(cols, data.split()))
+        if not interfaceName in self.interfaces:
+          self.interfaces[interfaceName] = InterfaceStats()
+        self.interfaces[interfaceName].update(now,
+                                              interfaceData['rx_bytes'],
+                                              interfaceData['tx_bytes'])
+
+
+# We use binary (not SI) prefixes.
+PREFIX_FACTOR = 1024
+PREFIXES = [' ', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
+
+def formatByteRate(byteRate):
+  """ Pretty-prints a bytes/second rate. The result will never exceed 3
+  characters in length.
+  """
+  byteRate = float(byteRate)
+  prefix = 0
+  while byteRate >= 99.5:
+    # The byteRate won't fit into 2 characters, so move to a higher prefix.
+    byteRate /= PREFIX_FACTOR
+    prefix += 1
+
+  if byteRate < 0.05:
+    byteRateStr = ' 0'
+  elif byteRate < 0.95:
+    byteRateStr = '.%d' % round(byteRate * 10)
+  elif byteRate < 9.5:
+    byteRateStr = ' %d' % round(byteRate)
+  else:
+    byteRateStr = '%d' % round(byteRate)
+
+  if prefix == 0:
+    return PREFIXES[prefix] + byteRateStr
+  else:
+    return byteRateStr + PREFIXES[prefix]
+
