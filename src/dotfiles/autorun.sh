ab35ff07e731a6ab81c5bd82f3b58b86c98ff6b0
pollen <pollen@localhost>
2014-03-17 08:31:37 -0500

    Moved gsettings command for marble mouse middle-click into autorun.sh.

--- src/dotfiles/autorun.sh
+++ src/dotfiles/autorun.sh
@@ -2,6 +2,13 @@
 # Standard stuff that should run when the user logs into a desktop session (i.e.
 # an i3 session).
 
+# This command is required to get middle-click functionality to work with the
+# Logitech Marble Mouse. See
+#   https://wiki.archlinux.org/index.php/Logitech_Marble_Mouse
+# TODO: verify that this doesn't mess up my laptop configuration
+gsettings set \
+  org.gnome.settings-daemon.peripherals.mouse middle-button-enabled true
+
 # Clear out the downloads folder.
 DOWNLOADS="${HOME}/Downloads"
 if [ -d "$DOWNLOADS" ]; then
