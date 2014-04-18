8f0236ebfd638da3d78de067e466139d3b7fb9e1
pollen <pollen@localhost>
2014-04-17 20:02:10 -0500

    Custom battery monitor for i3status.

--- src/python/powersupply.py
+++ src/python/powersupply.py
@@ -9,7 +9,7 @@ STATS_ROOT = '/sys/class/power_supply'
 
 
 def read(filename):
-  return open(filename, 'r').read()
+  return open(filename, 'r').read().strip()
 
 def convertMicroWattHoursToJoules(microWattHours):
   return microWattHours * 0.0036
@@ -23,6 +23,7 @@ class Stats:
   on instantiation, so you have to create a new Stats every time you want a
   fresh view. After instantiation, check out these fields:
     ac - Boolean indicating whether we are on AC power.
+    numBatteries - Number of batteries.
     batteryCapacity - Factory-rated battery capacity in Joules.
     batteryCharge - Current charge of battery in Joules.
     batteryPower - Current power output of battery in Watts.
@@ -30,7 +31,7 @@ class Stats:
 
   def __init__(self):
     self.ac = False
-    numBatteries = 0
+    self.numBatteries = 0
     self.batteryCapacity = 0
     self.batteryCharge = 0
     self.batteryPower = 0
@@ -39,15 +40,36 @@ class Stats:
       deviceRoot = path.join(STATS_ROOT, device)
       deviceType = read(path.join(deviceRoot, 'type'))
       if deviceType == 'Mains':
-        self.ac = True
+        if read(path.join(deviceRoot, 'online')) != '0':
+          self.ac = True
       elif deviceType == 'Battery':
+        self.numBatteries += 1
         self.batteryCapacity += convertMicroWattHoursToJoules(
             float(read(path.join(deviceRoot, 'energy_full_design'))))
         self.batteryCharge += convertMicroWattHoursToJoules(
             float(read(path.join(deviceRoot, 'energy_now'))))
-        delf.batteryPower += convertMicroWattsToWatts(
+        self.batteryPower += convertMicroWattsToWatts(
             float(read(path.join(deviceRoot, 'power_now'))))
 
-    if numBatteries == 0:
+    if self.numBatteries == 0:
       # Something must be powering the machine. Assume it's on AC.
       self.ac = True
+
+
+class StatsRefresher:
+  """ Provides a Stats object with bounded staleness. """
+
+  def __init__(self, refreshPeriod):
+    """ refreshPeriod is the maximum staleness, in seconds. """
+    self.refreshPeriod = refreshPeriod
+    self.lastRefresh = 0
+    self.stats = None
+
+  def get(self):
+    """ Gets a Stats. """
+    now = time.time()
+    if now - self.lastRefresh > self.refreshPeriod:
+      self.stats = Stats()
+      self.lastRefresh = now
+    return self.stats
+
