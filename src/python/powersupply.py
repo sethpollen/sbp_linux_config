54afcdec1d859b7823e62417a89fdb1ae894ccbf
pollen <pollen@localhost>
2014-04-19 07:16:17 -0500

    Added historical rate computations of battery discharge

--- src/python/powersupply.py
+++ src/python/powersupply.py
@@ -4,6 +4,7 @@
 import time
 import os
 import os.path as path
+from collections import deque
 
 STATS_ROOT = '/sys/class/power_supply'
 
@@ -29,7 +30,17 @@ class Stats:
     batteryPower - Current power output of battery in Watts.
   """
 
+  def __init__(self, ac, numBatteries, batteryCapacity, batteryCharge,
+               batteryPower):
+    """ Explicitly initializes the fields of this Stats. """
+    self.ac = ac
+    self.numBatteries = numBatteries
+    self.batteryCapacity = batteryCapacity
+    self.batteryCharge = batteryCharge
+    self.batteryPower = batteryPower
+
   def __init__(self):
+    """ Initializes the Stats from /sys. """
     self.ac = False
     self.numBatteries = 0
     self.batteryCapacity = 0
@@ -73,3 +84,44 @@ class StatsRefresher:
       self.lastRefresh = now
     return self.stats
 
+
+class HistoricalStatsRefresher:
+  """ Similar to StatsRefresher, but uses the historical values of the
+  batteries' energy level to compute power drain rate, instead of relying on
+  the "power_now" statistic in /sys. I have no idea what interval power_now is
+  computed, but it seems to be too short; power_no is too noisy.
+  """
+
+  def __init__(self, refreshPeriod, powerAggregatePeriod):
+    """ refreshPeriod is the maximum staleness, in seconds.
+    powerAggregatePeriod is the period to use when computing the power drain
+    rate.
+    """
+    self.refreshPeriod = refreshPeriod
+    self.powerAggregatePeriod = powerAggregatePeriod
+    self.lastRefresh = 0
+    self.stats = None
+    # chargeHistory is a queue of (time, batteryCharge) pairs.
+    self.chargeHistory = deque()
+
+  def takeSample(self, now):
+    """ Takes a new sample Stats right now. """
+    self.stats = Stats()
+    self.lastRefresh = now
+    self.chargeHistory.append((now, self.stats.batteryCharge))
+    # Garbage collect old data.
+    retainThreshold = now - self.powerAggregatePeriod
+    while self.chargeHistory[0][0] < retainThreshold:
+      self.chargeHistory.popleft()
+    # Compute the average power drain rate over the retained interval.
+    chargeDelta = (self.chargeHistory[0][1] - self.chargeHistory[-1][1])
+    timeDelta = (self.chargeHistory[-1][0] - self.chargeHistory[0][0])
+    if timeDelta > 0:
+      self.stats.batteryPower = max(0.0, chargeDelta / timeDelta)
+
+  def get(self):
+    """ Gets a Stats. """
+    now = time.time()
+    if now - self.lastRefresh > self.refreshPeriod:
+      self.takeSample(now)
+    return self.stats
