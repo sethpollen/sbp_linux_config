ff2393acca1d5be6600bf280057cef842bd90e83
seth <seth@localhost>
2014-09-29 09:54:03 -0500

    Marble mouse init logic from sjbrown

--- src/dotfiles/autorun.sh
+++ src/dotfiles/autorun.sh
@@ -15,6 +15,12 @@ if [ -d "$DOWNLOADS" ]; then
   mkdir "$DOWNLOADS"
 fi
 
+# Configure Logitech Marble Mouse.
+xinput set-int-prop "Logitech USB Trackball" "Evdev Wheel Emulation Button" 8 8
+xinput set-int-prop "Logitech USB Trackball" "Evdev Wheel Emulation" 8 1
+xinput set-int-prop "Logitech USB Trackball" "Evdev Middle Button Emulation" 8 1
+xinput set-prop "Logitech USB Trackball" "Evdev Wheel Emulation Axes" 6 7 4 5
+
 # Set up X key mappings.
 KEYMAP="${HOME}/.xkeymap"
 if [ -f "$KEYMAP" ]; then
