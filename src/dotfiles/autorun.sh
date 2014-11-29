1163d1ba0c6a48eab433889c3da8964a459d5eb5
seth <seth@localhost>
2014-11-29 10:23:14 -0600

    autorun.sh

--- src/dotfiles/autorun.sh
+++ src/dotfiles/autorun.sh
@@ -2,13 +2,6 @@
 # Standard stuff that should run when the user logs into a desktop session (i.e.
 # an i3 session).
 
-# Clear out the downloads folder.
-DOWNLOADS="${HOME}/Downloads"
-if [ -d "$DOWNLOADS" ]; then
-  rm -rf "$DOWNLOADS"
-  mkdir "$DOWNLOADS"
-fi
-
 # This command is required to get middle-click functionality to work with the
 # Logitech Marble Mouse. See
 #   https://wiki.archlinux.org/index.php/Logitech_Marble_Mouse
@@ -26,3 +19,13 @@ daemon unity-settings-daemon
 
 # Spawn a desktop widget for volume control. This runs as a daemon by default.
 kmix
+
+# Clear out the downloads folder.
+DOWNLOADS="${HOME}/Downloads"
+if [ -d "$DOWNLOADS" ]; then
+  rm -rf "$DOWNLOADS"
+  mkdir "$DOWNLOADS"
+fi
+
+# Prove that none of the above commands blocked.
+echo "Autoruns complete"
