d3c6137cae3ccf24d21f78bce7273113e64548ba
Seth Pollen <pollen@google.com>
2015-07-02 10:47:06 -0500

    Move super key fixes to desktop config only. They cause weird effects on the laptop keyboard.

--- src/dotfiles/autorun.sh
+++ src/dotfiles/autorun.sh
@@ -12,14 +12,6 @@ gsettings set \
 xmodmap -e "remove Lock = Caps_Lock"
 xmodmap -e "keysym Caps_Lock = Escape"
 
-# Ensure that the Ctrl keys on the Kinsesis keyboard (which map to Super_R and
-# Super_L are interpreted as Ctrl modifiers.
-# TODO: these may cause space to be eaten on my Thinkpad.
-#xmodmap -e "remove mod4 = Super_L"
-#xmodmap -e "remove mod4 = Super_R"
-#xmodmap -e "add control = Super_L"
-#xmodmap -e "add control = Super_R"
-
 # Now that we are done invoking gsettings, we can spawn a gnome-settings-daemon
 # to apply those changes. This also handles the laptop brightness and volume
 # keys.
