51b8a75bbde9e04ccb73e44a407e2e939944a376
pollen <seth.pollen@gmail.com>
2016-11-03 23:34:52 -0500

    fmt

--- sbpgo/games/words/wordlist_test.go
+++ sbpgo/games/words/wordlist_test.go
@@ -1,17 +1,17 @@
 package words_test
 
 import (
-  "testing"
+	"testing"
 )
 import . "github.com/sethpollen/sbp_linux_config/sbpgo/games/words"
 
 func TestFormatShortBytes(t *testing.T) {
-  list, err := GetWordlist()
-  if err != nil {
-    t.Error(err)
-    return
-  }
-  if len(list) != 5000 {
-    t.Errorf("Expected 5000 words; got %v", len(list))
-  }
+	list, err := GetWordlist()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(list) != 5000 {
+		t.Errorf("Expected 5000 words; got %v", len(list))
+	}
 }
