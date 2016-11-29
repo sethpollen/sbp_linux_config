86000f7cc558dd1e984e8af6c94588ea2fbe841e
pollen <seth.pollen@gmail.com>
2016-11-28 23:57:56 -0600

    gofmt

--- sbpgo/games/words/wiktionary/dump/extractor_main.go
+++ sbpgo/games/words/wiktionary/dump/extractor_main.go
@@ -4,32 +4,32 @@ package main
 
 import (
 	"encoding/xml"
-  "flag"
-  "io/ioutil"
-  "log"
-  "os"
-  "path"
-  "strings"
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
 )
 
 var inputFile = flag.String("input", "", "XML file to read")
 var outputDir = flag.String("output_dir", "",
-    "Directory to dump Module: page contents")
+	"Directory to dump Module: page contents")
 
 // XML struct.
 type Page struct {
-  Title string `xml:"title"`
-  Text string `xml:"revision>text"`
+	Title string `xml:"title"`
+	Text  string `xml:"revision>text"`
 }
 
 func main() {
 	flag.Parse()
-  if len(*inputFile) == 0 {
-    log.Fatalln("--input is required")
-  }
-  if len(*outputDir) == 0 {
-    log.Fatalln("--output_dir is required")
-  }
+	if len(*inputFile) == 0 {
+		log.Fatalln("--input is required")
+	}
+	if len(*outputDir) == 0 {
+		log.Fatalln("--output_dir is required")
+	}
 
 	file, err := os.Open(*inputFile)
 	if err != nil {
@@ -37,7 +37,7 @@ func main() {
 	}
 	decoder := xml.NewDecoder(file)
 
-	for i := 0;; i++ {
+	for i := 0; ; i++ {
 		rawToken, _ := decoder.Token()
 		if rawToken == nil {
 			break
@@ -46,21 +46,21 @@ func main() {
 		switch token := rawToken.(type) {
 		case xml.StartElement:
 			if token.Name.Local == "page" {
-        var page Page
-        decoder.DecodeElement(&page, &token)
-        
-        if strings.HasPrefix(page.Title, "Module:") {
-          outputFile := path.Join(*outputDir, page.Title)
-          
-          // TODO: this currently fails because some Module: titles have
-          // slashes.
-          err = ioutil.WriteFile(outputFile, []byte(page.Text), 0770)
-          if err != nil {
-            log.Fatalln(err)
-          }
-          
-          log.Println("Extracted ", page.Title)
-        }
+				var page Page
+				decoder.DecodeElement(&page, &token)
+
+				if strings.HasPrefix(page.Title, "Module:") {
+					outputFile := path.Join(*outputDir, page.Title)
+
+					// TODO: this currently fails because some Module: titles have
+					// slashes.
+					err = ioutil.WriteFile(outputFile, []byte(page.Text), 0770)
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					log.Println("Extracted ", page.Title)
+				}
 			}
 		}
 	}
