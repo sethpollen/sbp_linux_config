83818f89e5ab796ff57748d293bbf7987ae0463c
Seth Pollen <pollen@google.com>
2018-12-15 21:47:22 -0600

    buildifier

--- buildifier.sh
+++ buildifier.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 bazel run @com_github_bazelbuild_buildtools//buildifier -- \
-    $(find $HOME/sbp/sbp_linux_config -iname BUILD -type f)
+    $(find $HOME/sbp/sbp_linux_config -name "BUILD*" -type f)
