2f34aea1993cd6f66c4883264a7ce60c3780fbcd
Seth Pollen <pollen@google.com>
2019-04-26 11:31:38 -0500

    Simplify back

--- common-text/dotfiles/config/fish/completions/back.fish
+++ common-text/dotfiles/config/fish/completions/back.fish
@@ -1,9 +1,9 @@
 # If no subcommand has been given, suggest one.
 complete -f -c back \
-  -n "not string match -qr \"fork|join|ls|kill|peek\" (commandline -cpo)" \
-  -a "fork join ls kill peek"
+  -n "not string match -qr \"fork|join|ls\" (commandline -cpo)" \
+  -a "fork join ls"
 
 # Suggest job names for subcommands which take an existing job's name.
 complete -f -c back \
-  -n "string match -qr \"join|kill|peek\" (commandline -cpo)" \
+  -n "string match -qr \"join\" (commandline -cpo)" \
   -a "(back ls)"
