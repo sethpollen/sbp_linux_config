05d6fe674dcaa7e8ed028f8be58f1c49ec7399c2
Seth Pollen <pollen@google.com>
2019-12-24 22:24:23 -0600

    workspace

--- sbpgo/workspace.go
+++ sbpgo/workspace.go
@@ -1,50 +1,65 @@
 package sbpgo
 
 import (
-  "errors"
   "path"
 )
 
 // Workspace types.
 const (
-  NoWorkspace = iota
-  Git
+  Git = iota
   Hg
   G4
 )
 
 type WorkspaceInfo struct {
   Type int
+
+  // Path to the workspace root.
   Root string
+
+  // Path from the workspace root to the PWD. 'path.Join(Root, Path)' yields
+  // the original PWD.
+  Path string
 }
 
+// Returns nil if none of the workspace types matches.
 func FindWorkspace(pwd string, corp CorpContext) (*WorkspaceInfo, error) {
+  var info WorkspaceInfo
+  info.Root = pwd
+  info.Path = ""
+
   for ;; {
-    hg, err := DirExists(path.Join(pwd, ".hg"))
+    hg, err := DirExists(path.Join(info.Root, ".hg"))
     if err != nil {
       return nil, err
     }
     if hg {
-      return &WorkspaceInfo{Hg, pwd}, nil
+      info.Type = Hg
+      return &info, nil
     }
 
-    git, err := DirExists(path.Join(pwd, ".git"))
+    git, err := DirExists(path.Join(info.Root, ".git"))
     if err != nil {
       return nil, err
     }
     if git {
-      return &WorkspaceInfo{Git, pwd}, nil
+      info.Type = Git
+      return &info, nil
     }
 
-    if pwd == "/" {
-      return nil, errors.New("No workspace found")
-    }
-    dir := path.Dir(pwd)
-
-    if dir == corp.G4Root() {
-      return &WorkspaceInfo{G4, path.Base(pwd)}, nil
+    if info.Root == "/" {
+      // We searched the entire path and found no evidence of a workspace.
+      return nil, nil
     }
 
-    pwd = dir
+    // Shift one piece from the end of Root to the beginning of Path.
+    piece := path.Base(info.Root)
+    info.Root = path.Dir(info.Root)
+    info.Path = path.Join(piece, info.Path)
+
+    if info.Root == corp.G4Root() {
+      info.Type = G4
+      return &info, nil
+    }
   }
 }
