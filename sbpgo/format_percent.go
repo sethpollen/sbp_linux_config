0b35024c93281bbb72450a68bbf261f6fad4cc84
Seth Pollen <pollen@google.com>
2020-08-14 13:42:49 -0500

    format percent

---
+++ sbpgo/format_percent.go
@@ -0,0 +1,76 @@
+// Formats percentages as bar-charts for use in i3blocks status lines.
+
+package sbpgo
+
+import (
+	"flag"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+var label = flag.String("label", "",
+	"Label to prepend to the bar graph.")
+var keepNumber = flag.Bool("keep_number", false,
+	"If true, keep the numerical percentage too.")
+var history = flag.Int("history", 0,
+	"If positive, show a historical bar chart with this many bars.")
+var historyId = flag.String("history_id", "",
+	"A unique ID for history storage.")
+
+func FormatPercentMain() {
+	flag.Parse()
+	percentRe := regexp.MustCompile(" *([0-9]+\\.?[0-9]*)\\% *")
+
+	var text string = ReadStdin()
+
+	match := percentRe.FindStringSubmatch(text)
+	if match != nil {
+		var percentStr string = match[0]
+		percent, err := strconv.ParseFloat(match[1], 32)
+		if err != nil {
+			panic("main")
+		}
+		fraction := float32(percent / 100.0)
+
+		var graph string = *label
+		var newBar string = FractionToBar(fraction)
+
+		if *history > 1 && len(*historyId) > 0 {
+			// Ignore errors and fall back to an empty string.
+			graphHistoryBytes, _ := LoadShm(*historyId)
+			var graphHistory = string(graphHistoryBytes)
+			graphHistory += newBar
+
+			// Pad or trim to the desired size.
+			for utf8.RuneCountInString(graphHistory) < *history {
+				graphHistory = " " + graphHistory
+			}
+			for utf8.RuneCountInString(graphHistory) > *history {
+				_, width := utf8.DecodeRuneInString(graphHistory)
+				graphHistory = graphHistory[width:]
+			}
+
+			SaveShm(*historyId, []byte(graphHistory))
+			graph += graphHistory
+		} else {
+			graph += "▕" + newBar
+		}
+		graph += "▏"
+
+		if *keepNumber {
+			var percentInt = int64(percent)
+			// Make sure it fits into 2 digits.
+			if percentInt > 99 {
+				percentInt = 99
+			}
+			graph += fmt.Sprintf("%02d%%", percentInt)
+		}
+
+		text = strings.Replace(text, percentStr, graph, -1)
+	}
+
+	fmt.Print(text)
+}
