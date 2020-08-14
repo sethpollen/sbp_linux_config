f23e4d017d660537671ae81b5d7eff392bed3b0b
Seth Pollen <pollen@google.com>
2020-08-14 15:23:27 -0500

    forking over the last of common-text/python into Go

---
+++ sbpgo/dmenu.go
@@ -0,0 +1,27 @@
+// Library for invoking dmenu to get user input.
+
+package sbpgo
+
+import (
+  "os/exec"
+  "strings"
+)
+
+// Shows a dmenu with the given 'prompt' and 'options' to the user. Waits for
+// the user to respond. Returns the selected option, or "" if the user closed
+// the menu without choosing an option.
+func Dmenu(prompt string, options []string) (string, error) {
+  var argv []string
+  if len(prompt) > 0 {
+    argv = append(argv, "-p", prompt)
+  }
+
+  cmd := exec.Command("dmenu", argv...)
+  cmd.Stdin = strings.NewReader(strings.Join(options, "\n"))
+  result, err := cmd.Output()
+  if err != nil {
+    return "", err
+  }
+
+  return strings.TrimSpace(string(result)), nil
+}
