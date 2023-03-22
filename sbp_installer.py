5ba06384feb96ed0496540cf6acd6935fc3e74ec
Seth Pollen <seth.pollen@gmail.com>
2023-03-21 23:54:35 -0500

    The installer Python script seems fine now. It is way simpler and pretty easy to understand.

--- sbp_installer.py
+++ sbp_installer.py
@@ -8,21 +8,6 @@ import string
 import sys
 import subprocess
 
-# Build the set of paths used by sbp_linux_config during installation.
-
-HOME = os.getenv('HOME')
-assert len(HOME) > 0
-SBP = p.join(HOME, 'sbp')
-BIN = p.join(SBP, 'bin')
-
-# Directory for targets of dotfile symlinks under $HOME.
-DOTFILES_BIN = p.join(BIN, 'dotfiles')
-
-# Executables to be placed on $PATH.
-SCRIPTS_BIN = p.join(BIN, 'scripts')
-
-SBP_LINUX_CONFIG = p.join(SBP, 'sbp_linux_config')
-COMMON_TEXT = p.join(SBP_LINUX_CONFIG, 'common-text')
 
 def ForceLink(target, linkName):
   """ Forces a symlink, even if the linkName already exists. """
@@ -33,6 +18,7 @@ def ForceLink(target, linkName):
 
   print('Linking %s' % linkName)
   os.symlink(target, linkName)
+
 
 # Recursive helper for linking over individual files in the tree rooted at
 # dotfiles.
@@ -53,12 +39,15 @@ def LinkDotfiles(targetDir, linkDir, addDot):
       # Recurse, and don't add any more dots.
       LinkDotfiles(targetChild, linkChild, False)
 
+
 def StandardInstallation():
   """ Invokes the standard install procedure.
 
   1. Copies everything from ~/sbp/sbp_linux_config/common-text to ~/sbp/bin.
+
   2. Makes several symlinks in standard places (such as ~) that point
      to the appropriate files in ~/sbp/bin.
+
   3. If command-line arguments are provided, each is interpreted as a directory
      which may contain zero or more subdirectories corresponding to the
      subdirectories of ~/sbp/sbp_linux_config/common-text. Each file in each
@@ -68,12 +57,18 @@ def StandardInstallation():
      per-machine customizations.
   """
 
+  home = os.getenv('HOME')
+  assert len(home) > 0
+
+  sbp_bin = p.join(home, 'sbp', 'bin')
+  sbp_linux_config = p.join(home, 'sbp', 'sbp_linux_config')
+
   # Clean out any existing bin stuff.
-  if p.isdir(BIN):
-    shutil.rmtree(BIN)
+  if p.isdir(sbp_bin):
+    shutil.rmtree(sbp_bin)
 
   # Perform the copy.
-  shutil.copytree(COMMON_TEXT, BIN)
+  shutil.copytree(p.join(sbp_linux_config, 'common-text'), sbp_bin)
 
   # Process arguments to see if they contain append-files.
   appendDirs = sys.argv[1:]
@@ -83,14 +78,14 @@ def StandardInstallation():
     # Look at every file in the appendDir.
     for root, dirs, files in os.walk(appendDir):
       # Make root relative to the appendDir, since we'll want to use it both in
-      # the appendDir and in BIN.
+      # the appendDir and in sbp_bin.
       root = p.relpath(root, appendDir)
       for fil in files:
         # Compute the full path from the appendDir to the file.
         fil = p.join(root, fil)
 
         appendSource = p.join(appendDir, fil)
-        appendDest = p.join(BIN, fil)
+        appendDest = p.join(sbp_bin, fil)
 
         if p.exists(appendDest):
           print('Appending %s' % appendSource)
@@ -111,14 +106,15 @@ def StandardInstallation():
           shutil.copy(appendSource, appendDest)
 
   # Link over dotfiles.
-  LinkDotfiles(DOTFILES_BIN, HOME, True)
+  LinkDotfiles(p.join(sbp_bin, 'dotfiles'), home, True)
 
-  # Link in all the other scripts that should be on the path.
-  ForceLink(SCRIPTS_BIN, p.join(HOME, 'bin'))
+  # Link in all the other scripts that should be on the $PATH.
+  ForceLink(p.join(sbp_bin, 'scripts'), p.join(home, 'bin'))
 
   # Configure cron.
   print("Installing .crontab")
-  subprocess.call(['crontab', p.join(HOME, '.crontab')])
+  subprocess.call(['crontab', p.join(home, '.crontab')])
+
 
 if __name__ == "__main__":
     StandardInstallation()
