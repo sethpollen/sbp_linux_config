36869618454a3f5deea4ef31ff86df887e07e3fa
Seth Pollen <seth.pollen@gmail.com>
2023-03-21 23:09:52 -0500

    universal install script

--- install.sh
+++ install.sh
@@ -1,21 +1,31 @@
-#!/usr/bin/env bash
+#!/usr/bin/env fish
 #
 # Invokes the proper Python installer script for the current machine.
 
-hostname="$(hostname --short)"
-# Replace hyphens with underscores, as we can't use hyphens in Bazel directory
-# names if we wish to use Python.
-hostname="${hostname//-/_}"
+# Figure out what host we are installing on.
+set host (hostname --short)
 
+# Assume the host is a personal machine.
+set repo sbp
+
+# ... unless it matches one of my known corp hostnames.
+switch $host
+  case holroyd
+    set repo corp
+  case montero
+    set repo corp
+  case pollen
+    set repo corp
+end
+
+echo Installing from {$repo}_linux_config/hosts/{$host}.
+
+# Run the appropriate installer script for the host.
+cd $HOME/sbp/{$repo}_linux_config
+$HOME/sbp/tools/bazelisk run //hosts/{$host}:installer || exit 1
+
+# Install spbgo_main, which is the same for all hosts.
 cd $HOME/sbp/sbp_linux_config
-echo "Building installer..."
-~/sbp/tools/bazelisk build "//hosts/${hostname}:installer" || exit 1
+$HOME/sbp/tools/bazelisk run //sbpgo:deploy || exit 1
 
-cd bazel-bin
-echo "Running installer..."
-"hosts/${hostname}/installer"
-
-cd $HOME/sbp/sbp_linux_config
-~/sbp/tools/bazelisk run "//sbpgo:deploy" || exit 1
-
-echo "Installation complete."
+echo Installation complete.
