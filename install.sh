4b87a17cb44ff67d61109bff2c90b45ef1409e04
Seth Pollen <seth.pollen@gmail.com>
2023-03-21 23:41:04 -0500

    more simplifications to the installer

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #
 # Invokes the proper Python installer script for the current machine.
 
-set install $HOME/sbp/tools/bazelisk run //:sbp_installer
+set install $HOME/sbp/tools/bazelisk run //:sbp_installer --
 set corp $HOME/sbp/corp_linux_config
 
 # Prepare to invoke bazel.
@@ -11,13 +11,13 @@ cd $HOME/sbp/sbp_linux_config
 # Pass the appropriate set of input directories for each host.
 switch (hostname --short)
   case holroyd
-    $install $corp/common-text $corp/desktop-text $corp/hosts/holroyd/text
+    $install $corp/common $corp/workstation $corp/hosts/holroyd
 
   case montero
-    $install $corp/common-text $corp/desktop-text
+    $install $corp/common $corp/workstation
 
   case pollen
-    $install $corp/common-text $corp/hosts/pollen/text
+    $install $corp/common $corp/hosts/pollen
 
   case penguin
     $install
