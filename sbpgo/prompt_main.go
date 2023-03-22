839d224bf406dca62412f29fc70c2314d17ad3e9
Seth Pollen <seth.pollen@gmail.com>
2023-03-21 22:03:46 -0500

    bring over prompt_main.go from corp

--- sbpgo/prompt_main.go
+++ sbpgo/prompt_main.go
@@ -1,30 +1,117 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"github.com/sethpollen/sbp_linux_config/sbpgo"
+	"log"
+	"os/user"
+	"path"
+	"regexp"
+	"strings"
 )
 
-type DummyCorpContext struct{}
+type GoogleCorpContext struct {
+	User string
+}
 
-func (self DummyCorpContext) P4Root() *string {
-	return nil
+func pointerTo(s string) *string {
+	var p = new(string)
+	*p = s
+	return p
 }
-func (self DummyCorpContext) P4StatusCommand() *string {
-	return nil
+
+func (self GoogleCorpContext) P4Root() *string {
+	return pointerTo(path.Join("/google/src/cloud", self.User))
 }
-func (self DummyCorpContext) P4Status(
+
+func (self GoogleCorpContext) P4StatusCommand() *string {
+	return pointerTo("g4 pending")
+}
+
+func (self GoogleCorpContext) P4Status(
 	output []byte) (*sbpgo.WorkspaceStatus, error) {
-	return nil, nil
+	dirtyRegexp := regexp.MustCompile(
+		"^(Locally modified files)|(Default change :)")
+	pendingClRegexp := regexp.MustCompile(
+		"^Change [0-9]+ :")
+
+	var info sbpgo.WorkspaceStatus
+	var scanner = bufio.NewScanner(bytes.NewReader(output))
+
+	for scanner.Scan() {
+		if info.Dirty && info.PendingCl {
+			break
+		}
+		var line = scanner.Text()
+
+		if dirtyRegexp.MatchString(line) {
+			info.Dirty = true
+		}
+		if pendingClRegexp.MatchString(line) {
+			info.PendingCl = true
+		}
+	}
+
+	return &info, nil
 }
-func (self DummyCorpContext) HgLogCommand() *string {
-	return nil
+
+func (self GoogleCorpContext) HgLogCommand() *string {
+	// This is exactly what 'hg xl' does, except we don't pass -G and
+	// thus avoid getting ASCII art.
+	return pointerTo("hg log --rev smart --template google_compact")
 }
-func (self DummyCorpContext) HgLog(
+
+func (self GoogleCorpContext) HgLog(
 	output []byte) (*sbpgo.WorkspaceStatus, error) {
-	return nil, nil
+	var exportedAsRegexp = regexp.MustCompile("<exported as http://cl/[0-9]+>")
+
+	var info sbpgo.WorkspaceStatus
+	var scanner = bufio.NewScanner(bytes.NewReader(output))
+	var lineNumber = 1
+
+	for scanner.Scan() {
+		if info.Ahead && info.PendingCl {
+			break
+		}
+		if lineNumber%2 == 0 {
+			// Every second line is a commit description and can be dropped.
+			continue
+		}
+		lineNumber++
+
+		// Pad with a space to make it easier to match tokens at the end.
+		var line = scanner.Text() + " "
+
+		if strings.Contains(line, " p4head ") {
+			// Ignore this line; it is always present.
+			continue
+		}
+
+		// We have a commit, which is basically an unsubmitted CL.
+		info.PendingCl = true
+
+		if !exportedAsRegexp.MatchString(line) {
+			// This CL still needs to be exported.
+			info.Ahead = true
+		} else if strings.Contains(line, " orphan ") {
+			// We need to do some merges to get the exported CL back into a coherent
+			// state.
+			info.Ahead = true
+		}
+	}
+
+	return &info, nil
 }
 
 func main() {
-	var corp DummyCorpContext
+	u, err := user.Current()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var corp GoogleCorpContext
+	corp.User = u.Username
+
 	sbpgo.DoMain(corp)
 }
