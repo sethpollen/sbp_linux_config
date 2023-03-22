07b481b940c6544efd96afdc23e554437b4a2605
Seth Pollen <seth.pollen@gmail.com>
2023-03-22 00:32:26 -0500

    when installing, invoke bazel just one time

--- BUILD
+++ BUILD
@@ -1,8 +1,4 @@
 py_binary(
-    name = "sbp_installer",
-    srcs = ["sbp_installer.py"],
-    data = [
-        "//sbpgo:sbpgo_main",
-    ],
-    visibility = ["//visibility:public"],
+    name = "install",
+    srcs = ["install.py"],
 )
--- install.sh
+++ install.sh
@@ -2,32 +2,30 @@
 #
 # Invokes the proper Python installer script for the current machine.
 
-set bazel $HOME/sbp/tools/bazelisk
-
 # Build everything we'll need below.
 cd $HOME/sbp/sbp_linux_config
-$bazel build -c opt //:sbp_installer //sbpgo:sbpgo_main
+$HOME/sbp/tools/bazelisk build -c opt //:install //sbpgo:sbpgo_main
+cd ./bazel-bin
 
-set installer bazel-bin/sbp_installer
 set corp $HOME/sbp/corp_linux_config
 
 # Pass the appropriate set of input directories for each host.
 switch (hostname --short)
   case holroyd
-    $installer $corp/common $corp/workstation $corp/hosts/holroyd || exit 1
+    ./install $corp/common $corp/workstation $corp/hosts/holroyd || exit 1
 
   case montero
-    $installer $corp/common $corp/workstation || exit 1
+    ./install $corp/common $corp/workstation || exit 1
 
   case pollen
-    $installer $corp/common $corp/hosts/pollen || exit 1
+    ./install $corp/common $corp/hosts/pollen || exit 1
 
   case penguin
-    $installer || exit 1
+    ./install || exit 1
 end
 
 # Install spbgo_main, which is the same for all hosts.
 echo Copying sbpgo_main
-cp bazel-bin/sbpgo/sbpgo_main_/sbpgo_main $HOME/sbp/bin/scripts/
+cp ./sbpgo/sbpgo_main_/sbpgo_main $HOME/sbp/bin/scripts/
 
 echo Installation complete.
