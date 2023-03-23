c618df9906848fa8b4ce48364ec4173145653220
Seth Pollen <seth.pollen@gmail.com>
2023-03-22 23:44:52 -0500

    go installer

--- install.go
+++ install.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+  "bytes"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path"
 )
+
+const directoryMode = 0750
+const executableMode = 0550
 
 func main() {
 	homedir, err := os.UserHomeDir()
@@ -13,37 +18,51 @@ func main() {
 		log.Fatalln(err)
 	}
 
-  // Set up the destination directires which this installer will populate.
-	binScripts := path.Join(homedir, "sbp/bin/scripts")
-	err = os.MkdirAll(binScripts, 0750)
+	// Delete everything in the bin directory, so we can rebuild it from scratch.
+	bin := path.Join(homedir, "sbp/bin")
+	if err = os.RemoveAll(bin); err != nil {
+	  log.Fatalln(err)
+	}
+
+  // Set up the destination directories which this installer will populate.
+	binScripts := path.Join(bin, "scripts")
+	if err = os.MkdirAll(binScripts, directoryMode); err != nil {
+	  log.Fatalln(err)
+	}
+	binDotfiles := path.Join(bin, "dotfiles")
+	if err = os.MkdirAll(binDotfiles, directoryMode); err != nil {
+	  log.Fatalln(err)
+	}
+
+  // Install sbpgo_main, which is a data dependency of this program.
+	err = appendFile(
+	  "./sbpgo/sbpgo_main_/sbpgo_main", path.Join(binScripts, "sbpgo_main"),
+	  executableMode)
 	if err != nil {
 	  log.Fatalln(err)
 	}
 
-	err = installSbpgoMain(binScripts)
-	if err != nil {
-	  log.Fatalln(err)
-	}
+	// TODO: do the rest
 }
 
-func installSbpgoMain(binScripts string) error {
-  var err error
-
-	// Delete any existing sbpgo_main at the destination location.
-  dest := path.Join(binScripts, "sbpgo_main")
-  err = os.Remove(dest)
+// TODO: unit test this
+//
+// Copies from 'src' to 'dest', appending to any existing file.
+func appendFile(src string, dest string, perm os.FileMode) error {
+  // Read in the entire source file as a byte slice.
+  text, err := ioutil.ReadFile(src)
   if err != nil {
     return err
   }
 
-  // Read in the entire sbpgo_main binary as a byte slice.
-  text, err := ioutil.ReadFile("./sbpgo/sbpgo_main_/sbpgo_main")
+  // Open the file for append, creating it if it doesn't exist.
+  outFile, err := os.OpenFile(dest, os.O_APPEND|os.O_CREATE|os.O_WRONLY, perm)
   if err != nil {
     return err
   }
 
-  // Write the binary out to its new location.
-  err = ioutil.WriteFile(dest, text, 0750)
+  // Write out the file contents.
+  _, err = io.Copy(outFile, bytes.NewReader(text))
   if err != nil {
     return err
   }
