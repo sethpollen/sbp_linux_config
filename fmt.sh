ef2f564ae54a80f0d281e14b82ca49e252058b9d
Seth Pollen <seth.pollen@gmail.com>
2023-03-26 14:32:13 -0500

    Combine formatters

--- fmt.sh
+++ fmt.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# Auto-format all BUILD and Go files in this workspace.
+
+gofmt -w $HOME/sbp/sbp_linux_config
 
 $HOME/sbp/tools/buildifier \
   $(find $HOME/sbp/sbp_linux_config -name "BUILD*" -type f)
--- gofmt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gofmt -w $HOME/sbp/sbp_linux_config
