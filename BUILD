50ad1e783dff9472d8b1909c31a07cece78b6b2d
Seth Pollen <seth.pollen@gmail.com>
2023-03-26 13:01:56 -0500

    rename

--- BUILD
+++ BUILD
@@ -1,7 +1,7 @@
 load("@io_bazel_rules_go//go:def.bzl", "go_binary")
 
 go_binary(
-    name = "install",
-    srcs = ["install.go"],
+    name = "install_main",
+    srcs = ["install_main.go"],
     data = ["//sbpgo:sbpgo_main"],
 )
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd $HOME/sbp/sbp_linux_config
-$HOME/sbp/tools/bazelisk run -c opt //:install || exit 1
+$HOME/sbp/tools/bazelisk run -c opt //:install_main || exit 1
 
 # TODO: Automatically invoke desktop setup for desktops.
